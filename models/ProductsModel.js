13302dce1d0f1064e9515ddc3a2a806ae04932b1
eunbiryung <eunbiryung@gmail.com>
2018-06-12 12:26:05 +0900

    'basic'

---
+++ models/ProductsModel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+
+
+var ProductsSchema = new Schema({
+    name : String, 
+    price : Number,
+    description : String, 
+    created_at : { 
+        type : Date,
+        default : Date.now()
+    }
+});
+
+ProductsSchema.virtual('getDate').get(function(){
+    var date = new Date(this.created_at);
+    return {
+        year : date.getFullYear(),
+        month : date.getMonth()+1,
+        day : date.getDate()
+    };
+});
+
+
+ProductsSchema.plugin( autoIncrement.plugin , { model : 'products' , field : 'id' , startAt : 1 });
+module.exports = mongoose.model('products', ProductsSchema);
