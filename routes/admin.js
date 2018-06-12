94a323457304beeb6838d990226409aade754cf2
eunbiryung <eunbiryung@gmail.com>
2018-06-12 17:51:46 +0900

    '댓글작성'

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ProductsModel = require('../models/ProductsModel');
+var CommentsModel = require('../models/CommentsModel');
 
 router.get('/', function(req,res){
     res.send('admin app');
 });
 
 router.get('/products', function(req,res){
-    ProductsModel.find(function(err,products){
+    ProductsModel.find( function(err,products){ //첫번째 인자는 err, 두번째는 받을 변수명
         res.render( 'admin/products' , 
             { products : products } // DB에서 받은 products를 products변수명으로 내보냄
         );
@@ -33,7 +34,10 @@ router.post('/products/write', function(req,res){
 router.get('/products/detail/:id' , function(req, res){
     //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
     ProductsModel.findOne( { 'id' :  req.params.id } , function(err ,product){
-        res.render('admin/productsDetail', { product: product });  
+        //제품정보를 받고 그안에서 댓글을 받아온다.
+        CommentsModel.find({ product_id : req.params.id } , function(err, comments){
+            res.render('admin/productsDetail', { product: product , comments : comments });
+        });        
     });
 });
 
@@ -59,10 +63,24 @@ router.post('/products/edit/:id', function(req, res){
 });
 
 router.get('/products/delete/:id', function(req, res){
-    ProductsModel.remove({ id : req.params.id}, function(err){
+    ProductsModel.remove({ id : req.params.id }, function(err){
         res.redirect('/admin/products');
     });
 });
 
+router.post('/products/ajax_comment/insert', function(req,res){
+    var comment = new CommentsModel({
+        content : req.body.content,
+        product_id : parseInt(req.body.product_id)
+    });
+    comment.save(function(err, comment){
+        res.json({
+            id : comment.id,
+            content : comment.content,
+            message : "success"
+        });
+    });
+
+});
 
 module.exports = router;
